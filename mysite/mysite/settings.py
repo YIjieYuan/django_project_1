b91728426d7f13ffd3f147294f22d26484e9cc76
yijie yuan <yijiey17@gmail.com>
2019-10-07 20:46:22 -0400

    步入正轨

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_project_1',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'PORT': '3306',
+        'HOST': '127.0.0.1',
+
+
+
     }
 }
 
