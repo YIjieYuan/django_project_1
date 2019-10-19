59c47a000e1733fe208706b95f30ebadb22ad267
yijie yuan <yijiey17@gmail.com>
2019-10-19 13:56:30 -0400

    20191019 1354

--- mysite/notes/views.py
+++ mysite/notes/views.py
@@ -31,35 +31,51 @@ def notes_index(request):
             category_set.append(note.category)
     category_set.sort(key=str.lower)
 
+    # 建立一个三级字典
+    # 第三级：sup_category
     sup_category_set = []
-    sup_category_number = 1
     for note in note_set:
+        # 第二级：Category
         category_list = []
-        category_number = 1
         if note.sup_category not in [item['name'] for item in sup_category_set]:
             for note_sup in Note.objects.filter(sup_category=note.sup_category):
+                # 第一级：note
                 note_list = []
                 if note_sup.category not in [item['name'] for item in category_list]:
+                    # add info to note_list
+                    # 因为note这里已经是模型的最底层对象了，所以可以用一些API的操作，不用和上级一样对字典排序
                     for note_cat in Note.objects.filter(category=note_sup.category).order_by('-pub_date')[:5]:
                         note_list.append({'title': note_cat.title,
                                           'category': note_cat.category,
-                                          'slug': note_cat.slug})
+                                          'slug': note_cat.slug
+                                          })
 
-                    category_list.append({'id': category_number,
-                                          'name': note_sup.category,
-                                          'note_list': note_list})
-                    category_number += 1
-            category_list.sort(key=itemgetter('id'))
-            sup_category_set.append({'id': sup_category_number,
-                                     'name': note.sup_category,
-                                     'category_list': category_list})
-            sup_category_number += 1
-    sup_category_set.sort(key=itemgetter('id'))
+                    # add info to category_list
+                    category_list.append({'name': note_sup.category,
+                                          'note_list': note_list
+                                          })
+            # category排序方式：按照name排序
+            category_list.sort(key=itemgetter('name'))
+            # 给name排序之后的每个category加一个顺序id
+            category_number = 1
+            for category in category_list:
+                category['id'] = category_number
+                category_number += 1
+
+            # add info to sup_category_set
+            sup_category_set.append({'name': note.sup_category,
+                                     'category_list': category_list,
+                                     'max_category_id': max([item['id'] for item in category_list])
+                                     })
+
+    # sup_category排序方式：按照name排序
+    sup_category_set.sort(key=itemgetter('name'))
+    sup_category_number = 1
+    for sup_category in sup_category_set:
+        sup_category['id'] = sup_category_number
+        sup_category_number += 1
 
     context = {}
-    for category in category_set:
-        context[category] = Note.objects.filter(category=category).order_by('-pub_date')[:5]
-    context['category_set'] = category_set
     context['sup_category_set'] = sup_category_set
 
     return render(request, 'notes/index.html', context=context)
---
+++ update log.md
@@ -0,0 +1,6 @@
+20191019
+
+13:54
+
+1. 修复了note category排版的bug
+2. 让notes app中的category和sup_catehgory按照字母顺序排列
