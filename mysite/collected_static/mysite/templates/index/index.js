d41a97231ea82af042cfcca2bb565653afa94c8c
yijie yuan <yijiey17@gmail.com>
2019-11-01 12:05:48 -0400

    更新collected_static

---
+++ mysite/collected_static/mysite/templates/index/index.js
@@ -0,0 +1,13 @@
+$(function(){
+    $(".typed-quotes").typed({
+        strings: ["Hi！你来了，",
+            "I am Yijie, a student in MedPhy",
+            "Here I'm sharing my life !",
+            "Hope you will <span style='color:#d14836'>❤</span> IT."],
+        typeSpeed: 50,
+        contentType: 'html',
+        loop: false,
+        showCursor: false,
+        backDelay: 1000
+    });
+});
